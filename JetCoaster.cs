413dd149bbd6985e0cab7bbe2f3d1e8c9287ebb9
jc21-190798 <75291204+jc21-190798@users.noreply.github.com>
2022-02-18 14:28:19 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace WindowsFormsApp1
+{
+    public class JetCoaster
+    {
+
+
+
+        public static int ride(int k, int[] g, int r)
+        {
+            int[] jet = new int[g.Length];
+            Array.Copy(g, jet, g.Length);
+            int goukei = 0;
+            int hozon;
+            int aa = k;
+
+
+
+            for (int n = 0; r > n; n++)
+            {
+                k = aa;
+                int count = 0;
+                for (int i = 0; i < g.Length; i++)
+                {
+                    if (k >= g[i])
+                    {
+                        k = k - g[i];
+                        goukei = goukei + g[i];
+                        count++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                for (int c = 0; count > c; c++)
+                {
+                    hozon = g[0];
+                    for (int s = 0; g.Length > s; s++)
+                    {
+                        if (s == g.Length - 1)
+                        {
+                            g[s] = hozon;
+                        }
+                        else
+                        {
+                            g[s] = g[s + 1];
+                        }
+                    }
+                }
+            }
+            Array.Copy(jet, g, jet.Length);
+            return goukei;
+        }
+    }
+}
---
+++ kuriage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class kuriage
+    {
+        public static int kuriage10(int i)
+        {
+            decimal a = i;
+            int b;
+            if (a >= 0)
+            {
+                b = (int)((Math.Ceiling(a / 10)) * 10);
+            }
+            else
+            {
+                b = (int)((Math.Floor(a / 10)) * 10);
+            }
+            return b;
+        }
+    }
+}
